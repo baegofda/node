793b38d68d05ad4af630f75dfe54f27ba288c231
baegofda <molly.daleo3o@gmail.com>
2021-07-28 21:31:52 +0900

    express basic

---
+++ node/14-express/app-route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import postRouter from "./router/post.js";
+import userRouter from "./router/user.js";
+const app = express();
+
+app.use(express.json()); //REST API -> Body
+app.use(express.urlencoded({ extended: false })); // HTML Form -> Body
+app.use(express.static("public")); //
+app.use("/posts", postRouter);
+app.use("/users", userRouter);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`success ${port} ~!!!!!!!!!!!!!!!!!!!!`));
--- node/14-express/app.js
+++ node/14-express/app.js
@@ -1,25 +1,24 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
 const app = express();
+const corsOptions = {
+  origin: ["http://127.0.0.1:5500"],
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+app.use(express.json());
+app.use(cookieParser()); // 쿠키를 사용하기위해
+app.use(morgan("combined")); // 서버의 요청을 편하게 확인이 가능하다
+app.use(helmet()); // 공통적으로 필요한 보안에 관련된 헤더들을 추가할 수 있다
+app.use(cors(corsOptions));
 
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("first");
-    next();
-  },
-  (req, res, next) => {
-    console.log("first2");
-    res.send("hghhhh");
-  }
-);
-
-app.use((req, res, next) => {
-  res.status(404).send("없는 페이지 !!");
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send("sry ~! ");
+app.get("/", (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies);
+  res.send("Welcome!");
 });
 
 const port = process.env.PORT || 8080;
